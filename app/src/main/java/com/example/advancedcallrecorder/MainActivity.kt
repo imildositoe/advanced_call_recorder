23227673fbba5b3e7b6c119599200afbc9d805b1
poisonshade <imildositoe@gmail.com>
2022-10-29 22:51:15 +0200

    MainActivity initialized with Call Recorder function. Plugins imported

--- app/src/main/java/com/example/advancedcallrecorder/MainActivity.kt
+++ app/src/main/java/com/example/advancedcallrecorder/MainActivity.kt
@@ -1,11 +1,30 @@
 package com.example.advancedcallrecorder
 
+import android.media.MediaRecorder
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import com.aykuttasil.callrecord.CallRecord
 
 class MainActivity : AppCompatActivity() {
+
+    companion object {
+        private val TAG = MainActivity::class.java.simpleName
+    }
+
+    private lateinit var callRecord: CallRecord
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+//        callRecord = CallRecord.init(this)
+
+        callRecord = CallRecord.Builder(this)
+            .setLogEnable(true)
+            .setRecordFileName("CallRecorderTestFile")
+            .setRecordDirName("CallRecorderTest")
+            .setAudioSource(MediaRecorder.AudioSource.VOICE_COMMUNICATION)
+            .setShowSeed(true)
+            .build()
     }
 }
